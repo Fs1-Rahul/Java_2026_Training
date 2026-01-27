85e0846a807399a3549b5e9a8765056dd0c830ac
Fs1-Rahul <ragoel@ciena.com>
2026-01-27 08:42:19 +0000

    Date and Time API practice using tutorialspoint

--- 27-01-26/Streamspractice.java
+++ 27-01-26/Streamspractice.java
@@ -1,14 +1,11 @@
-import java.util.HashSet;
+
 import java.util.List;
-import java.util.Set;
+
 import java.util.stream.Stream;
 public class Streamspractice {
     public static void main(String[] args) {
         System.out.println("This is a placeholder for Streamspractice class.");
         Stream<String> stream = Stream.of("A", "B", "C");
-        Stream<String> stream1;
-        Stream<String> stream2;
-        Stream<String> stream3;
         Stream<List<String>> listStream = Stream.of(
             List.of("cAc", "azBc"),
             List.of("arCc", "gDc"),
